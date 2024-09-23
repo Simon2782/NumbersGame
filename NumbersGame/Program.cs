c987ee07464334e26079bd09b7a90252a49aeeda
sgust <sgust@STRONGUZI>
2024-09-23 09:50:29 +0200

    Add project files.

---
+++ NumbersGame/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace NumbersGame //Simon Gustafsson SUT24
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+            int rInt = r.Next(1, 20);
+            Console.WriteLine("Välkommen! Jag tänker på ett nummer. Kan du gissa vilket? Du får fem försök.");
+            int userChoice = Convert.ToInt32(Console.ReadLine());
+        }
+
+        
+    }
+}
